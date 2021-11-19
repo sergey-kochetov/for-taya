3ee9d27398913ddfd5f8abac0d8cbcbb324f2e99
KochetovSV <git.kochetov@gmail.com>
2021-11-19 14:10:35 +0300

    init master

---
+++ src/main/java/ru/kochetov/spring_rb/SpringRbApplication.java
@@ -0,0 +1,13 @@
+package ru.kochetov.spring_rb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kochetov/spring_rb/SpringRbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kochetov.spring_rb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
